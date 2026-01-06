d8ab9647a140ee7c223aa6a7e3c0c83362ba9eca
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2026-01-06 13:55:17 -0500

    find session api progress

--- server/internal/session/db.ts
+++ server/internal/session/db.ts
@@ -66,5 +66,8 @@ export default function createDBSessionHandler(): SessionProvider {
         },
       });
     },
+    async findSessions(_options) {
+      return [];
+    },
   };
 }
--- server/internal/session/types.d.ts
+++ server/internal/session/types.d.ts
@@ -14,10 +14,19 @@ export type Session = {
   };
 };
 
+export interface SessionFindOptions {
+  userId?: string;
+  data: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
+  };
+}
+
 export interface SessionProvider {
   getSession: <T extends Session>(token: string) => Promise<T | undefined>;
   setSession: (token: string, data: Session) => Promise<Session | undefined>;
   updateSession: (token: string, data: Session) => Promise<boolean>;
   removeSession: (token: string) => Promise<boolean>;
   cleanupSessions: () => Promise<void>;
+  findSessions: (options: SessionFindOptions) => Promise<Session[]>;
 }
